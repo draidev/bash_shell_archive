2b091701e0ff3ea298a64ff726487eee0275fb2f
draidev <drtkdldjstm@naver.com>
2023-08-11 16:57:16 +0900

    modify lai_day_statistics.sh : 데이터 단위 추가 및 print_stat함수 작성

--- sh/lai_day_statistics.sh
+++ sh/lai_day_statistics.sh
@@ -44,39 +44,35 @@ sum_hour_size() {
         ((chatgpt_hour_size+=data_size))
         #echo -e "chatgpt_hour_size : ${chatgpt_hour_size}"
     fi
+
+    ((hour_total_size=exec_hour_size+image_hour_size+unknown_hour_size+post_hour_size+chatgpt_hour_size))
+}
+
+print_stat() {
+    echo "#$1 : $2"
+    echo "#$3 : $4B ($(($4/1024))K)($(($4/1024/1024))M)($(($4/1024/1024/1024))G)"
+    ((aver=$4/$2))
+    echo "#average_$3 : ${aver}B ($(($aver/1024))K)($(($aver/1024/1024))M)($(($aver/1024/1024/1024))G)"
 }
 
 process_after_one_hour() {
     if [ $exec_hour_cnt -gt 0 ]; then
-        echo "#exec_hour_cnt : $exec_hour_cnt"
-        echo "#exec_hour_size : $exec_hour_size($((exec_hour_size/1024))K)"
-        ((average_exec_hour_size=exec_hour_size/exec_hour_cnt))
-        echo "#average_exec_hour_size : $average_exec_hour_size($(($average_exec_hour_size/1024))K)"
+        print_stat exec_hour_cnt $exec_hour_cnt exec_hour_size $exec_hour_size
     fi
     if [ $image_hour_cnt -gt 0 ]; then
-        echo "#image_hour_cnt : $image_hour_cnt"
-        echo "#image_hour_size : $image_hour_size($((image_hour_size/1024))K)"
-        ((average_image_hour_size=image_hour_size/image_hour_cnt))
-        echo "#average_image_hour_size : $average_image_hour_size($((average_image_hour_size/1024))K)"
+        print_stat image_hour_cnt $image_hour_cnt image_hour_size $image_hour_size
     fi 
     if [ $unknown_hour_cnt -gt 0 ]; then
-        echo "#unknown_hour_cnt : $unknown_hour_cnt"
-        echo "#unknown_hour_size : $unknown_hour_size(((unknown_hour_size/1024))K)"
-        ((average_unknown_hour_size=unknown_hour_size/unknown_hour_cnt))
-        echo "#average_unknown_hour_size : $average_unknown_hour_size($((average_unknown_hour_size/1024))K)"
+        print_stat unknown_hour_cnt $unknown_hour_cnt unknown_hour_size $unknown_hour_size
     fi
     if [ $post_hour_cnt -gt 0 ]; then
-        echo "#post_hour_cnt : $post_hour_cnt"
-        echo "#post_hour_size : $post_hour_size($((post_hour_size/1024))K)"
-        ((average_post_hour_size=post_hour_size/post_hour_cnt))
-        echo "#average_post_hour_size : $average_post_hour_size($((average_post_hour_size/1024))K)"
+        print_stat post_hour_cnt $post_hour_cnt post_hour_size $post_hour_size
     fi
     if [ $chatgpt_hour_cnt -gt 0 ]; then
-        echo "#chatgpt_hour_cnt : $chatgpt_hour_cnt"
-        echo "#chatgpt_hour_size : $chatgpt_hour_size($((post_hour_size/1024))K)"
-        ((average_chatgpt_hour_size=chatgpt_hour_size/chatgpt_hour_cnt))
-        echo "#average_chatgpt_hour_size : $average_chatgpt_hour_size($((average_chatgpt_hour_size/1024))K)"
+        print_stat chatgpt_hour_cnt $chatgpt_hour_cnt chatgpt_hour_size $chatgpt_hour_size
     fi    
+
+    echo "[ hour_total_size : ${hour_total_size}B ($((hour_total_size/1024))K)($((hour_total_size/1024/1024))M)($((hour_total_size/1024/1024/1024))G) ]"
 
     ((exec_day_cnt+=exec_hour_cnt))
     ((image_day_cnt+=image_hour_cnt))
@@ -129,10 +125,14 @@ unknown_day_size=0
 post_day_size=0
 chatgpt_day_size=0
 
+hour_total_size=0
+day_total_size=0
+
 for i in {00..23}; do  # loop per hour
+    echo "<<process Hour : $i>>"
     for j in {00..15} 98; do # loop per thread number
         for t in "${file_items[@]}"; do
-            ls -al $default_path$1_$i/00/$j/$t.files 2> /dev/null
+            #ls -al $default_path$1_$i/00/$j/$t.files 2> /dev/null
             file_size=$(ls -al $default_path$1_$i/00/$j/$t.files 2> /dev/null | cut -d ' ' -f5)
             if [ -e $default_path$1_$i/00/$j/$t.meta ] && [ $(wc -l < $default_path$1_$i/00/$j/$t.meta 2>/dev/null) -gt 0 ]; then
                 file_count=$(cat $default_path$1_$i/00/$j/$t.meta | wc -l)
@@ -141,7 +141,7 @@ for i in {00..23}; do  # loop per hour
             fi
         done
         for t in "${post_items[@]}"; do
-            ls -al $default_path$1_$i/00/$j/$t.data 2> /dev/null
+            #ls -al $default_path$1_$i/00/$j/$t.data 2> /dev/null
             data_size=$(ls -al $default_path$1_$i/00/$j/$t.data 2> /dev/null | cut -d ' ' -f5)
             if [ -e $default_path$1_$i/00/$j/$t.meta ] && [ $(wc -l < $default_path$1_$i/00/$j/$t.meta 2>/dev/null) -gt 0 ]; then
                 data_count=$(cat $default_path$1_$i/00/$j/$t.meta | wc -l) 
@@ -157,28 +157,21 @@ done
 # print all
 echo "###############################"
 if [ $exec_day_cnt -gt 0 ]; then
-    echo "#exec_day_cnt : $exec_day_cnt"
-    echo "#exec_day_size: $((exec_day_size/1024))K"
-    echo -e "#average_exec_day_size: $((exec_day_size / exec_day_cnt))\n"
+    print_stat exec_day_cnt $exec_day_cnt exec_day_size $exec_day_size
 fi
 if [ $image_day_cnt -gt 0 ]; then
-    echo "#image_day_cnt : $image_day_cnt"
-    echo "#image_day_cnt : $((image_day_size/1024))K"
-    echo -e "#average_image_day_size : $((image_day_size / image_day_cnt))\n"
+    print_stat image_day_cnt $image_day_cnt image_day_size $image_day_size
 fi
 if [ $unknown_day_cnt -gt 0 ]; then
-    echo "#unknown_day_cnt : $unknown_day_cnt"
-    echo "#unknown_day_size : $((unknown_day_size/1024))K"
-    echo -e "#average_unknown_day_size : $((unknown_day_size / unknown_day_cnt))\n"
+    print_stat unknown_day_cnt $unknown_day_cnt unknown_day_size $unknown_day_size
 fi
 if [ $post_day_cnt -gt 0 ]; then
-    echo "#post_day_cnt : $post_day_cnt"
-    echo "#post_day_size : $((post_day_size/1024))K"
-    echo "#average_post_day_size : $((post_day_size / post_day_cnt))"
+    print_stat post_day_cnt $post_day_cnt post_day_size $post_day_size
 fi
 if [ $chatgpt_day_cnt -gt 0 ]; then
-    echo "#chatgpt_day_cnt : $chatgpt_day_cnt"
-    echo "#chatgpt_day_size : $((chatgpt_day_size/1024))K"
-    echo "#average_chatgpt_day_size : $(($chatgpt_day_size / chatgpt_day_cnt))"
+    print_stat chatgpt_day_cnt $chatgpt_day_cnt chatgpt_day_size $chatgpt_day_size
 fi
+
+((day_total_size=exec_day_size+image_day_size+unknown_day_size+post_day_size+chatgpt_day+size))
+echo "[ day_total_size : $((day_total_size/1024))K, $((day_total_size/1024/1024))M, $((day_total_size/1024/1024/1024))G ]"
 echo "###############################"
